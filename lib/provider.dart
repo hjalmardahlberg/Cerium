13d950d7c35bfebac2415a4b97f96440d0add844
unknown <viktorkangasniemi@gmail.com>
2023-03-29 23:23:31 +0200

    now prints out access token in terminal

--- lib/provider.dart
+++ lib/provider.dart
@@ -2,8 +2,13 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
+//import 'package:googleapis/calendar/v3.dart';
+
+
 class GoogleSignInProvider extends ChangeNotifier{
   final googleSignIn = GoogleSignIn();
+    //scopes: ["https://www.googleapis.com/auth/calendar"],
+  //);
 
   GoogleSignInAccount? _user;
 
@@ -12,10 +17,15 @@ class GoogleSignInProvider extends ChangeNotifier{
   Future googleLogin() async{
     try {
       final googleUser = await googleSignIn.signIn();
+
       if (googleUser == null) return;
       _user = googleUser;
 
       final googleAuth = await googleUser.authentication;
+
+      String? accessToken = googleAuth.accessToken;
+      print("ACCESS TOKEN:");
+      print(accessToken);
 
       final credential = GoogleAuthProvider.credential(
         accessToken: googleAuth.accessToken,
